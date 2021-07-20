be9977b69986067361600a3b8b07f86d946b3590
Nikita Esyunin <hikita.yess@gmail.com>
2021-07-20 19:02:53 +0500

    Add login and register
    without design

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -15,4 +15,25 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def menu = Action {
     Ok(views.html.menu())
   }
+
+  def auth = Action { implicit request =>
+    Ok(views.html.auth())
+  }
+
+  def login = Action { implicit request =>
+    request.body.asFormUrlEncoded.map { args =>
+      val username = args("username").head
+      val password = args("password").head
+      Redirect(routes.HomeController.auth).flashing("success" -> "Вы вошли в свой аккаунт")
+    }.getOrElse(Redirect(routes.HomeController.login))
+  }
+
+  def register = Action { implicit request =>
+    request.body.asFormUrlEncoded.map { args =>
+      val username = args("username").head
+      val password = args("password").head
+      Redirect(routes.HomeController.auth).flashing("success" -> "Вы зарегестрировали свой аккаунт")
+    }.getOrElse(Redirect(routes.HomeController.login))
+
+  }
 }
